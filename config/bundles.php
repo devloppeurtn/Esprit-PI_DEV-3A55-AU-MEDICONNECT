97ce060f52293e1b347370dc2eebcb0d069e3705
Houssem Hfaissi <houssemhfaissi8@gmail.com>
2026-05-12 14:04:39 +0100

    fix: add KnpUOAuth2ClientBundle to bundles.php

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,7 @@ return [
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
